fa9b2f65406f9034ec71e0e74c464568de458d80
Joseph Tessel <Tessel.Joseph@gmail.com>
2016-04-03 15:10:40 -0400

    Prevent User From Signing Up Multiple Times

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,6 @@ class Event < ActiveRecord::Base
   belongs_to :user
   has_many :guestlists, dependent: :destroy
   validates :user, presence: true
-  acts_as_followable
+  
+
 end
--- app/models/guestlist.rb
+++ app/models/guestlist.rb
@@ -2,4 +2,5 @@ class Guestlist < ActiveRecord::Base
   belongs_to :event, dependent: :destroy
   belongs_to :user, dependent: :destroy
   validates :user_id, :presence => true
+  validates :user, :uniqueness => { :scope => :event, :message => "You can't sign up for an event twice!!!" }
 end
