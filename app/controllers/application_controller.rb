ddb8fd03d5ebff97daf56f656994ca5c1dd5443c
Joseph Tessel <Tessel.Joseph@gmail.com>
2016-04-10 16:18:54 -0400

    can't log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,26 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
  protected
-
  def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name
+   devise_parameter_sanitizer.for(:sign_up) << :phone_number
+   devise_parameter_sanitizer.for(:sign_up) << :first_name
+   devise_parameter_sanitizer.for(:sign_up) << :last_name
+ end
+ def after_sign_in_path_for(resource)
+ sign_in_url = new_user_session_url
+   if request.referer == sign_in_url
+     super
+   else
+     stored_location_for(resource) || request.referer || root_path
+   end
+ end
+
+ def after_sign_out_path_for(resource)
+   sign_in_url = new_user_session_url
+   if request.referer == sign_in_url
+     super
+   else
+     stored_location_for(resource) || request.referer || root_path
+   end
  end
 end
